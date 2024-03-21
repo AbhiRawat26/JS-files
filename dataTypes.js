38214a1feccaa0fffff467ad996db04f53cd5504
Ankit Rawat <ankitrawat@Ankits-MacBook-Pro.local>
2024-03-21 17:25:09 +0530

    data types

---
+++ dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) //we are using nodejs, not browser
+
+console.log(3 
+    + 3) //code readability should be high
+
+console.log("abhishek")
+
+let name = "ankit"
+let age = 20
+let isLoggedIn = false
+let state;
+
+// number =>2 to power 53
+// bigint
+// string=> ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined);
